93cfbc8128b24c2f0d6e44506915c4820bc6c4b4
rafaeltei-xeira <rafateimarques2@gmail.com>
2025-02-28 10:57:18 -0300

    Add files via upload

---
+++ scripts.js
@@ -0,0 +1,7 @@
+document.addEventListener("DOMContentLoaded", function() {
+    document.getElementById("redirectBtn").onclick = function() {
+        window.location.href = "https://www.instagram.com/rafael.teixeira06/";
+    };
+});
+
+
